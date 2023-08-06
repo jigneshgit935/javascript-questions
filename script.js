ad9518ca884da9f7674410daed7559d323bf5abc
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-07 03:26:06 +0530

    some examples related to hoisting

--- script.js
+++ script.js
@@ -1,49 +1,82 @@
-// Hoisting= it is js concept , jo variable declaration and functions ko top pr move krdeta h - to the top of the scopee
-// hoisting concept works with only var, not with let and const
-// var
-// let
-// const
+// hoisting a  javascript mechansim
+// two phase one is (creation) where we declare all (hoisting is done in creation phase where js compiler see all the declaration) and second is (execution) where our code runs
 
 // example 1
-// {var name = 'Jignesh Sharma';
-// var name = 'Jinu Sharma';
-// console.log(name)};
+// console.log(name);
+// name = 'Junu sharma';
 
 // example 2
-
-//We have 2 hoisting ,  one is partially hoisted and second one is fully hoisted = var we have are partially hoisted and functions we have fully hoisted
-// 1.Partially , it moves var to top and assigned it to undefined
-// 2.Fully , it moves  to top and assigned the actual value under functions
-
-// {console.log(name());
-// function name() {
-//   console.log('Sharma jignesh');
-//   return true;
-// }
+// var name = 'Jinu';
+// console.log(name);
+// var name = 'Sharma';
+// console.log(name);
 
 // example 3
-
-// console.log(name());
-// (function name() {
-//   console.log('Sharma jignesh');
-//   return true;
-// });
-
-// its output will give us error , that name is not a function ,becouse it doesnt found function,hoisting want a function should be a start at any cost
-// it means line ka first word should be function but we have assigned () this bracket  or any const, is this hoisting will not work anyway, so its showing us error
+// var name = undefined;
+// name = 'Jinu';
+// console.log(name);
+// name = 'Sharma';
+// console.log(name);
 
 // example 4
 
-// when we declare any function into an variable we call it function expression
-
-// console.log(name());
-// const name = () => {
-//   console.log('Jinu sharma');
-// };
-
+// var name = 'Jinu';
+// var name = 'Sharma';
 // console.log(name);
-// var name = () => {   this will give us undefined becoz we have a var as a variable, becoz it is a partially hoisted
-//   console.log('Jinu sharma');
-// };
 
-// here even if we put var let const , it will give us error becoz hoisting requires function word should be
+// example 5
+
+// var name = 'Jinu';
+// let name = 'Sharma';
+// console.log(name);
+
+// it will give us error becoz of let
+
+// example 6
+// name();
+// function name() {
+//   console.log('Jignesh');
+// }
+
+// example 7
+// name();
+// function name() {
+//   console.log('Jignesh');
+// }
+
+// name();
+// function name() {
+//   console.log('Sharma');
+// }
+
+// example 8
+// function name() {
+//   console.log('Jignesh');
+// }
+
+// function name() {
+//   console.log('Sharma');
+// }
+// name();
+
+// example 9
+// function name() {
+//   console.log('Jignesh');
+// }
+// name();
+
+// function name() {
+//   console.log('Sharma');
+// }
+// name();
+
+// example 10
+function name() {
+  console.log('Jignesh');
+}
+
+function name() {
+  console.log('Sharma');
+}
+name();
+name();
