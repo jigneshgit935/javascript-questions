da33554ac7de5b581e0c7f7123f1a1040d070f00
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-07 02:25:38 +0530

    recursive fn to asynchronus

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-console.log('1');
-console.log('2');
-setTimeout(() => console.log('3'), 1000);
-setTimeout(() => console.log('4'), 5000);
-console.log('5');
+function removeItem() {
+  console.log('javascript');
+  setTimeout(() => removeItem(), 0);
+  //   removeItem(); //recusrion function
+}
 
-// javascript-script-engines , asynschronos concepts
-// set timeout is an asynchronos function   == js engine does not execute
-// js runtime executes asynchronos set timeout
-// in js runtime k andr ek concept which we call it webapi ,which handles our asynchronus request
+removeItem();
